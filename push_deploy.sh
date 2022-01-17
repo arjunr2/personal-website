2ca2676a5c4be151c6b8268b27f81df27ac84bfe
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-01-17 13:13:08 +0530

    Added deploy error checks

--- push_deploy.sh
+++ push_deploy.sh
@@ -5,9 +5,18 @@ BASE_DIR=$(dirname "$0")
 
 # Push updates and deploy gh-pages
 git -C $BASE_DIR push
+if [ $? -ne 0]; then
+  echo "Failed script"
+  exit 1
+fi
+
 sleep 5
 
 # Update CMU website (rebase needed for forced gh-page push)
-ssh afs 'git -C pull $REMOTE_DIR --rebase'
+ssh afs 'git -C $REMOTE_DIR pull --rebase'
+if [ $? -ne 0]; then
+  echo "Failed script"
+  exit 1
+fi
 
 echo "Completed deployment!"
