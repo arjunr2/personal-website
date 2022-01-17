d4adf41ab1017b13ccfa7eb371c1ecedb985b1f3
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2022-01-17 13:25:10 +0530

    Fixed script bugs

--- push_deploy.sh
+++ push_deploy.sh
@@ -4,17 +4,23 @@ REMOTE_DIR=/afs/ece.cmu.edu/usr/arjunr2/public_html
 BASE_DIR=$(dirname "$0")
 
 # Push updates and deploy gh-pages
+set -x
 git -C $BASE_DIR push
-if [ $? -ne 0]; then
+set +x 
+
+if [ $? -ne 0 ]; then
   echo "Failed script"
   exit 1
 fi
 
-sleep 5
+sleep 1
 
 # Update CMU website (rebase needed for forced gh-page push)
-ssh afs 'git -C $REMOTE_DIR pull --rebase'
-if [ $? -ne 0]; then
+set -x
+ssh afs "git -C $REMOTE_DIR pull --rebase"
+set +x
+
+if [ $? -ne 0 ]; then
   echo "Failed script"
   exit 1
 fi
